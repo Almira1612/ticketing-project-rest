677fdb9fd60453c7be6e8764e2be55c76e405505
Aimila Maimaitijiang <almira1612@gmail.com>
2022-12-19 21:57:55 -0500

    Adding packages

--- src/main/java/com/cydeo/TicketingProjectRestApplication.java
+++ src/main/java/com/cydeo/TicketingProjectRestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingProjectRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketingProjectRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestApplication.class, args);
+    }
 
 }
