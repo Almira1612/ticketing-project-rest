7cdd30fa6f1757b9b39296722d8dde3f3fc37607
Aimila Maimaitijiang <almira1612@gmail.com>
2023-03-14 22:50:48 -0400

    Final edit

--- src/main/java/com/cydeo/TicketingProjectRestApplication.java
+++ src/main/java/com/cydeo/TicketingProjectRestApplication.java
@@ -1,7 +1,11 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketingProjectRestApplication {
@@ -10,4 +14,13 @@ public class TicketingProjectRestApplication {
         SpringApplication.run(TicketingProjectRestApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
